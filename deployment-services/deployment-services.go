54477a37977acf4af038848028c2d0ba5aa5c28a
tluwaniMS <tluwani2020@gmail.com>
2024-01-26 17:05:38 +0200

    adding-depoloyment-object-creation

--- deployment-services/deployment-services.go
+++ deployment-services/deployment-services.go
@@ -2,5 +2,42 @@ package deployment_service
 import ("kube-api-comms/deployment_type")
 
 func GenerateDeploymentObject (deployment string,replicas int,image string) {
-	deploymentObject := deployment_type.
+	deploymentObject := deployment_type.Deployment{
+		apiVersion: "apps/v1",
+		kind: "Deployment" ,
+		metadata:{
+			name:deployment,
+			labels:{
+				app:"nginx"
+			}
+		},
+		spec:{
+			replicas:replicas,
+			selector:{
+				matchLabels:{
+					app:"nginx"
+				}
+			},
+			template:{
+				metadata:{
+					labels:{
+						app: "nginx"
+					}
+				},
+				spec:{
+					containers:[
+						{
+							name: deployment + " - container",
+							image: image,
+							ports:[
+								{containerPort:8080}
+							]
+						}
+					]
+				}
+			}
+		}
+	}
+
+	
 }

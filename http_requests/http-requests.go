7b8fae8a7ebef173a04fae624665223be23b4d12
tluwaniMS <tluwani2020@gmail.com>
2024-01-27 05:49:08 +0200

    updating-http-request-generator

--- http_requests/http-requests.go
+++ http_requests/http-requests.go
@@ -2,6 +2,7 @@ package http_requests
 
 import (
 	"bytes"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -11,26 +12,49 @@ func GetClient() *http.Client {
 	return client
 }
 
-func GenerateGetRequest(endPoint string) []byte {
-	req, err := http.NewRequest(http.MethodGet, endpoint)
+func GenerateGetRequest(endPoint string) *http.Request {
+	req, err := http.NewRequest(http.MethodGet, endPoint)
 
 	if err != nil {
 		log.Fatalf("Error Occurred. %+v", err)
 	}
+
+	return req
 }
 
-func GeneratePutRequest(endPoint string, jsonData []byte) []byte {
-	req, err := http.NewRequest(http.MethodPut, endpoint, bytes.NewBuffer(jsonData))
+func GeneratePutRequest(client *http.Client, endPoint string, jsonData []byte) *http.Request {
+	req, err := http.NewRequest(http.MethodPut, endPoint, bytes.NewBuffer(jsonData))
 
 	if err != nil {
 		log.Fatalf("Error Occurred. %+v", err)
 	}
+
+	return req
 }
 
-func GeneratePostRequest(endPoint string, jsonData []byte) []byte {
-	req, err := http.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer(jsonData))
+func GeneratePostRequest(client *http.Client, endPoint string, jsonData []byte) *http.Request {
+	req, err := http.NewRequest(http.MethodPost, endPoint, bytes.NewBuffer(jsonData))
 
 	if err != nil {
 		log.Fatalf("Error Occurred. %+v", err)
 	}
+
+	return req
+}
+
+func MakeHttpRequest(client *http.Client, request *http.Request) []byte {
+	response, err := client.Do(request)
+
+	if err != nil {
+		log.Fatalf("Error sending request to API endpoint. %+v", err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalf("Couldn't parse response body. %+v", err)
+	}
+
+	return body
 }

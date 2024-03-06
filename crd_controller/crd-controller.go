8f0cf30a2aeda2649d708ad009a04baf28d2a0e1
tluwaniMS <tluwani2020@gmail.com>
2024-03-06 10:36:12 +0200

    adding-crd-controller

--- crd_controller/crd-controller.go
+++ crd_controller/crd-controller.go
@@ -1,6 +1,12 @@
 package crd_controller
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"kube-api-comms/crd_controller_type"
+	"kube-api-comms/http_requests"
+	"kube-api-comms/kube_api_requests"
 	"net/http"
 )
 
@@ -8,4 +14,26 @@ func GetCrds(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "application/json")
 	response.WriteHeader(http.StatusOK)
 
+	crd := mux.Vars(request)["deployment"]
+
+	client := http_requests.GetClient()
+
+	kubeApiEndPoint := kube_api_requests.GenerateCrdApi(crd)
+	kubeRequest := http_requests.GenerateGetRequest(kubeApiEndPoint)
+
+	kubeResponseBody := http_requests.MakeHttpRequest(client, kubeRequest)
+
+	var kubeResponseObject map[string]interface{}
+
+	error := json.Unmarshal(kubeResponseBody, &kubeResponseObject)
+
+	if error != nil {
+		fmt.Println("There was an error unmarshalling the body.")
+	}
+
+	responseMessage := crd_controller_type.CrdGetResponse{Crd: kubeResponseObject}
+
+	message, _ := json.Marshal(responseMessage)
+
+	response.Write(message)
 }
---
+++ crd_controller_type/crd-controller-type.go
@@ -0,0 +1,5 @@
+package crd_controller_type
+
+type CrdGetResponse struct {
+	Crd map[string]interface{} `json:"crd"`
+}
